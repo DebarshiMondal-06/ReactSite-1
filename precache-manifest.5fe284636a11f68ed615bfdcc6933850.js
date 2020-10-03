0443d6b1218ef541726688245df1121f9cebc9f2
Debarshi <debopiku1122@gmail.com>
2020-10-03 10:41:18 +0530

    Updates

---
+++ precache-manifest.5fe284636a11f68ed615bfdcc6933850.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d9b88309ace9765f1ae4bfffd50deb",
+    "url": "/ReactSite-1/index.html"
+  },
+  {
+    "revision": "19157de27bf41b88b433",
+    "url": "/ReactSite-1/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "0f4fa95c09a678ee0c66",
+    "url": "/ReactSite-1/static/css/main.c69566fd.chunk.css"
+  },
+  {
+    "revision": "19157de27bf41b88b433",
+    "url": "/ReactSite-1/static/js/2.4cb446a4.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/ReactSite-1/static/js/2.4cb446a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4fa95c09a678ee0c66",
+    "url": "/ReactSite-1/static/js/main.2f3fbe45.chunk.js"
+  },
+  {
+    "revision": "46f2833b3953b5b5ba9b",
+    "url": "/ReactSite-1/static/js/runtime-main.c11f498a.js"
+  },
+  {
+    "revision": "970b859d45689c78bf928ca2dc38b710",
+    "url": "/ReactSite-1/static/media/s1.970b859d.jpg"
+  },
+  {
+    "revision": "59fe410036da3b0653ffbe633137f59e",
+    "url": "/ReactSite-1/static/media/s2.59fe4100.jpg"
+  },
+  {
+    "revision": "bd17a3436a475e3b948d35ca4cb14071",
+    "url": "/ReactSite-1/static/media/s3.bd17a343.jpg"
+  },
+  {
+    "revision": "86ef88ccb9ca5b45930b4b8b025b3042",
+    "url": "/ReactSite-1/static/media/s4.86ef88cc.jpg"
+  },
+  {
+    "revision": "d62ad47e43de6058e6d2e69e5db695cc",
+    "url": "/ReactSite-1/static/media/s5.d62ad47e.jpg"
+  }
+]);
