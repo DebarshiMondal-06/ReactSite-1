40991dc9bd90ede8532dd2b346d916a1d0dded19
Debarshi <debopiku1122@gmail.com>
2021-07-11 23:12:59 +0530

    referesh code-3

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -4,7 +4,7 @@ import Common from "./Common";
 const Home = () => {
     return (
             <Common
-                name='We are the team of three!'
+                name='We are the team of two!'
                 visit='/service'
                 btname="Get Message"
             />
